ed04ce460a883451dac23da0af4582a1d90ca3f3
Joe O'Donovan <jojoodonovan@hotmail.com>
2023-09-22 14:40:36 +0100

    addingURL

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+// Async function to retrieve current weather temperature asynchronously using the await function waiting for porblems to resolv.
+async function retrieveTemperature() {
+  // Using fetch function to make a GET request the API URL. Using await to pause execution until promise returned by fetch resolves.
+    "https://api.open-meteo.com/v1/forecast?latitude=51.5&longitude=0.119999886&current_weather=true"
+  );
+  const data = await response.json();
+  // Here we are storing the response in a variable.
+  const temperature = data.current_weather.temperature;
+ // Implmenting the variable int our web page.
+  const keyElement = document.getElementById("key");
+  keyElement.textContent = temperature;
+}
 
-   // Asynchronously retrieves a joke and displays it
-async function GetandDisplayWeather() {
-    const degrees = await retrieveDegrees();
-    displayWeather(degrees);
-  }
-  // Function to asynchronously retrieve a random joke from an API
-  async function retrieveDegrees() {
-    // Send a GET request to the dad joke API. Await the response.
-    // Declare a variable to store the HTTP response
-    const response = fetch("https://api.open-meteo.com/v1/forecast?current_weather=true", {
-        headers: {
-          Accept: "application/json",
-        }})
-       console.log(response);
+retrieveTemperature();
 
     // Check if the response failed, and if so log an error and halt the app
     if (!response.ok) {
@@ -20,18 +19,4 @@ async function GetandDisplayWeather() {
       console.error(`Text: ${await response.text()}`);
       return;
     }
-    // return the parsed JSON from the response (which contains the joke object)
-    const data = await response.json();
-    return data;
-  }
-  // Function to update the DOM with the provided joke
-  function displayWeather(degrees) {
-    const weatherElement = document.getElementById("degrees");
-    jokeElement.textContent = degrees.joke;
-  }
-  // Waits for the DOM to be fully loaded and then displays an initial joke.
-  //document.addEventListener("DOMContentLoaded", getAndDisplayNewJoke);
-  // Retrieves the "new joke" button
- // const newJokeButton = document.getElementById("newJokeBtn");
-  // Sets up a click event listener to fetch and display a new joke upon clicking the newJokeButton.
-  //newJokeButton.addEventListener("click", getAndDisplayNewJoke);
+   
--- temperature.js
+++ temperature.js
@@ -1,65 +1,24 @@
 // Asynchronously retrieves a temperature and displays it
+// 1.We have fetched temputure from API server.
+// 2.We have located where we can access it from.
+// 3.We have tried to log that data to browswer console and not getting anything in return.
+// 4. Figure out why we cant log to browswer
+// 
+
 async function getAndDisplayNewWeather() {
-    const degrees = await retrieveDegrees();
-    displayWeather(degrees);
-  }
-  
-  // Function to asynchronously retrieve temperature
-  async function retrieveDegrees() {
-    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=0.1272&current_weather=true", {
-      headers: {
-        Accept: "application/json",
-      },
-    });
+  const degrees = await retrieveDegrees();
+  displayWeather(degrees);
+}
+// Async function to retrieve current weather temperature
+async function retrieveTemperature() {
+  const response = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=51.5&longitude=0.119999886&current_weather=true"
+  );
+  const data = await response.json();
+  const temperature = data.current_weather.temperature;
 
-    const C = response.current_weather.temperature(0)
-    console.log(C); // Log out the response
-    // ... Rest of the code
-  }
+  const keyElement = document.getElementById("key");
+  keyElement.textContent = temperature;
+}
 
-
-
-
-
-/* Asynchronously retrieves a temperature and displays it
-async function getAndDisplayNewWeather() {
-    const degrees = await retrieveDegrees();
-    displayWeather(degrees);
-  }
-
-  // Function to asynchronously retrieve temperature
-  async function retrieveDegrees() {
-    // Send a GET request to the dad joke API. Await the response.
-    // Declare a variable to store the HTTP response
-    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=0.1272&current_weather=true", {
-      headers: {
-        Accept: "application/json",
-      },
-    });
-
-    // Check if the response failed, and if so log an error and halt the app
-    if (!response.ok) {
-      console.error(`Status: ${response.status}`);
-      console.error(`Text: ${await response.text()}`);
-      return;
-    }
-
-    // return the parsed JSON from the response (which contains the joke object)
-    const data = await response.json();
-    return data;
-  }
-
-  // Function to update the DOM with the provided joke
-  function displayWeather(degrees) {
-    const weatherElement = document.querySelector(".key");
-    weatherElement.textContent = degrees.temperature;
-  }
-
-  // Waits for the DOM to be fully loaded and then displays an initial joke.
-  //document.addEventListener("DOMContentLoaded", getAndDisplayNewJoke);
-
-  // Retrieves the "new joke" button
-  //const newJokeButton = document.getElementById("newJokeBtn");
-
-  // Sets up a click event listener to fetch and display a new joke upon clicking the newJokeButton.
-  //newJokeButton.addEventListener("click", getAndDisplayNewJoke);
+retrieveTemperature();
